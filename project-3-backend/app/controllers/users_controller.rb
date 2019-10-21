ffdffe8226c38af8e6e4f691343ed71d1720b902
github username <github email address>
2019-10-21 14:32:02 -0500

    added login

--- project-3-backend/app/controllers/users_controller.rb
+++ project-3-backend/app/controllers/users_controller.rb
@@ -4,4 +4,10 @@ class UsersController < ApplicationController
         users = User.all
         render json: users.to_json
     end
+    
+    def create
+        user = User.create(username: params[:username])
+        render json: user.to_json
+    end
+
 end
--- project-3-frontend/src/index.js
+++ project-3-frontend/src/index.js
@@ -1,8 +1,49 @@
+let CURRENT_USER = '';
+
 document.addEventListener('DOMContentLoaded', function(){
-    fetchPage()
+    while(CURRENT_USER == ''){
+        signIn();
+    }
+    fetchUser(CURRENT_USER);
 })
 
-function fetchPage(){
+function signIn() {
+    var person = prompt("Please enter your name", "Your Name Here");
+    if (person != null) {
+      CURRENT_USER = person;
+    }
+}
+
+function fetchUser(username){
+    fetch('http://localhost:3000/users')
+    .then(res => res.json())
+    .then(data => {
+        data.forEach(user => {
+        if(user.username == CURRENT_USER){
+            CURRENT_USER = user;
+            fetchAllPosts();
+        }
+        })
+        if(typeof CURRENT_USER == "string"){
+            fetch('http://localhost:3000/users', {
+                method: 'POST',
+                headers: {
+                    "Content-Type": "Application/JSON"
+                },
+                body: JSON.stringify({username: CURRENT_USER})
+            })
+            .then(res => res.json())
+            .then(data => {
+                CURRENT_USER = data;
+                fetchAllPosts();
+            })
+        }
+    })
+}
+
+function fetchAllPosts(){
+    let h = document.querySelector('#greeting');
+    h.innerHTML = `Let's get this bread, ${CURRENT_USER.username}!`;
     fetch('http://localhost:3000/posts')
     .then(res => res.json())
     .then(data => data.forEach(post => {

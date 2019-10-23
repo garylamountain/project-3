c709821448f4d426a740a3e35f1a472969071edb
github username <github email address>
2019-10-23 15:38:44 -0500

    added report feature

--- project-3-backend/app/controllers/posts_controller.rb
+++ project-3-backend/app/controllers/posts_controller.rb
@@ -11,14 +11,13 @@ class PostsController < ApplicationController
 
     def update
         post = Post.find_by(id: params[:id])
-        post.update(likes: params[:likes])
+        post.update(likes: params[:likes], is_reported: params[:is_reported])
         render json: post.to_json(:include => :comments)
     end
 
     def create
         user = User.find_by(id: params[:user][:id])
-        post = Post.create(src: params[:src], caption: params[:caption], likes: params[:likes], user: user)
-        p post
+        post = Post.create(src: params[:src], caption: params[:caption], likes: params[:likes], user: user, is_reported: false)
         render json: post.to_json
     end
 
